738db0c0c9d60821354129be84bfe1ddcc0d0b6b
Erickson Leonel Castillo Navarro <ericksonc@uninorte.edu.co>
2021-09-21 18:00:07 -0500

    elimine codigo

--- app.py
+++ app.py
@@ -10,13 +10,3 @@ else:
     print("El numero ", numero, " no es primo")
 
 
-numero=int(input("Digite un numero: "))
-
-cont = 0
-for i in range(1, numero + 1):
-    if numero % i == 0:
-        cont = cont +1
-if cont == 2:
-    print("El numero ", numero, " es primo")
-else:
-    print("El numero ", numero, " no es primo")
