6a7d9be4315f9dc1a584b3eaea7c938e4a327e5b
Erickson Leonel Castillo Navarro <ericksonc@uninorte.edu.co>
2021-09-21 17:54:13 -0500

    puse mas codigo

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ if cont == 2:
     print("El numero ", numero, " es primo")
 else:
     print("El numero ", numero, " no es primo")
+
+
+numero=int(input("Digite un numero: "))
+
+cont = 0
+for i in range(1, numero + 1):
+    if numero % i == 0:
+        cont = cont +1
+if cont == 2:
+    print("El numero ", numero, " es primo")
+else:
+    print("El numero ", numero, " no es primo")
