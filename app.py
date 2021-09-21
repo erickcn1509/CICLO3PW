18d95011b3118f7bb0b0256fec44e07366ce9f7a
Erickson Leonel Castillo Navarro <ericksonc@uninorte.edu.co>
2021-09-21 17:18:17 -0500

    no he podido solucionar esto

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 numero=input("Digite un numero: ")
 
-print("El numero es ", numero)
+cont = 0
+for i in range(numero):
+    if numero % 2 == 0:
+        cont = cont +1
+if cont == 2:
+    print("El numero ", numero, " es primo")
+else:
+    print("El numero ", numero, " no es primo")
